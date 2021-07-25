e0fbbeba85a68d7a32417bdd2171abd185f1908f
Yiklek <yiklek.me@gmail.com>
2021-07-25 14:37:27 +0800

    优化

--- tool/tool.py
+++ tool/tool.py
@@ -4,66 +4,96 @@ import argparse
 import re
 import sys
 
-
-def openfly(in_path, out_path):
-    path = in_path
-    out_path = os.path.abspath(out_path)
-    config_base_dir = os.path.abspath(os.path.dirname(path))
+def load_rime_dict(path, code2word, word2code):
     line_pattern = re.compile(r"^(.*?)\s+(.*?)\s*(\d*)\s*$")
+    try:
+        f = open(path, 'r')
+        file_content = f.read()
+        file_split = file_content.split("...")
+        _file_config = file_split[0]
+        file_dict = file_split[1]
+        file_line = file_dict.splitlines()
+        for l in file_line:
+            m = line_pattern.match(l)
+            if m:
+                word = m.group(1)
+                code = m.group(2)
+                if word and code:
+                    code2word[code] = code2word.get(code, [])
+                    code2word[code].append(word)
+                    word2code[word] = code2word.get(word, [])
+                    word2code[word].append(code)
+                    word2code[word] = sorted(word2code[word]) 
+    except Exception as e:
+        print("load table {} ocurred error skip.{}".format(path, e), 
+                file=sys.stderr)
+    finally:
+        if f is not None and not f.closed:
+            f.close()
+
+def load_rime(path):
+    config_base_dir = os.path.abspath(os.path.dirname(path))
     config = None
     with open(path, 'r') as f:
         config = yaml.load(f, Loader=yaml.FullLoader)
     tables = config["import_tables"]
-    all_word_dict = dict()
+    code2word = dict()
+    word2code = dict()
     for t in tables:
-        try:
-            f = open(os.path.join(config_base_dir, t+'.dict.yaml'), 'r')
-            file_content = f.read()
-            file_split = file_content.split("...")
-            file_config = file_split[0]
-            file_dict = file_split[1]
-            file_line = file_dict.splitlines()
-            for l in file_line:
-                m = line_pattern.match(l)
-                if m:
-                    word = m.group(1)
-                    code = m.group(2)
-                    if word and code:
-                        all_word_dict[code] = all_word_dict.get(code, [])
-                        all_word_dict[code].append(word)
-        except Exception as e:
-            print("convert table {} ocurred error skip.{}", t, e)
-        finally:
-            if f is not None and not f.closed:
-                f.close()
-    sorted_keys = sorted(all_word_dict.keys())
-    output = open(out_path, 'w')
-    for k in sorted_keys:
-        output.write("{} {}\n".format(k, " ".join(all_word_dict[k])))
-    output.close()
+        load_rime_dict(os.path.join(config_base_dir, t +
+                       '.dict.yaml'), code2word, word2code)
+    return code2word, word2code
+
+
+def rime2zf(path):
+    code2word, word2code = load_rime(path)
+    sorted_keys = sorted(code2word.keys())
+    return ["{} {}".format(k, " ".join(code2word[k])) for k in sorted_keys]
+
+
+def output(out, out_path, outstd):
+    out_file = None
+    if out_path is not None:
+        out_file = open(out_path, 'w')
+    for k in out:
+        if out_file is not None:
+            out_file.write(k + '\n')
+        if outstd:
+            print(k, file=sys.stdout)
+    if out_file is not None and not out_file.closed:
+        out_file.close()
+
     print("{} records converted. write to {}".format(
-        len(all_word_dict), out_path))
+        len(out), out_path or 'stdout' if outstd else None), file=sys.stderr)
 
 
-def build(parser, args):
-    if getattr(args, 'from') == 'openfly':
-        openfly(args.input, args.output)
-
+def convert(parser, args):
+    result = None
+    if getattr(args, 'from') == 'rime' and getattr(args, 'to') == 'zf':
+        result = rime2zf(args.input)
+    
+    if result is not None:
+        output(result, args.output, args.outstd)
 
 def create_arg_parser():
     parser = argparse.ArgumentParser(description='build tool.')
+
     subparsers = parser.add_subparsers(metavar="COMMAND", dest='command')
-    # install
-    parser_install = subparsers.add_parser(
-        'build', aliases=['b'], help='build db file')
-    parser_install.set_defaults(func=build)
-    parser_install.add_argument('from', metavar='FROM', type=str, choices=['openfly'],
-                                default='openfly', help='from format. options: openfly. ')
-    parser_install.add_argument('-i', '--input', dest='input', type=str, default='openfly.dict.yaml',
+    # convert
+    parser_convert = subparsers.add_parser(
+        'convert', aliases=['conv'], help='convert dict file')
+    parser_convert.set_defaults(func=convert)
+    parser_convert.add_argument('from', metavar='FROM', type=str, choices=['rime'],
+                                default='rime', help='from format. options: rime. ')
+    parser_convert.add_argument('to', metavar='TO', type=str, choices=['zf'],
+                                default='zf', help='to format. options: zf. ')
+    parser_convert.add_argument('-i', '--input', dest='input', type=str, default='openfly.dict.yaml',
                                 action='store', help='input file')
 
-    parser_install.add_argument('-o', '--output', dest='output', type=str, default='openfly.txt',
+    parser_convert.add_argument('-o', '--output', dest='output', type=str, default=None,
                                 action='store', help='output file')
+    parser_convert.add_argument('-outstd', '--output-std', dest='outstd', default=False,
+                               action='store_true', help='output stdout')
 
     return parser
 
